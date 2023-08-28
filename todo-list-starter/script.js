4ff75f7361b99bcb57f4c955a7aa3e3d2861a2c9
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-08-28 17:54:17 +1000

    Completing baseline tasks for to do list

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,54 +1,61 @@
 const toDoList = ["Update resume", "Binge junk food", "have a nap"];
-const toDoStatus = [false,true,false,true]
+const toDoTaskStatus = [false,true,false]
+updateTodoList();
+
 function addTask() {
   
   // Get the item value 
   const newToDo = document.getElementById("new-task-text");
-  //  Push this item to my toDoList
-  toDoList.push(newToDo.value);
 
-  // Update to do list
-  updateTodoList();
-
-  // clear my input field
-  newToDo.value = "";
+  if (newToDo.value) {
+    //  Push this item to my toDoList
+    toDoList.push(newToDo.value);
+    // Push incomplete status to toDoTaskStatus
+    toDoTaskStatus.push(false);
+    // clear my input field
+    newToDo.value = "";
+    // Update to do list
+    updateTodoList();
+  }
 }
 
 function updateTodoList() {
   const toDoListElement = document.getElementById("todo-list");
   toDoListElement.innerHTML = "";
-  for (const [index, toDo] of toDoList.entries()) {
-    const newToDoText = document.createElement("p");
-    newToDoText.innerHTML = toDo;
-
-    const markCompleteButton = document.createElement("input");
-    markCompleteButton.type = "button";
-    markCompleteButton.onclick = function() {
-      toggleComplete(index);
-    };
-    markCompleteButton.value = "Complete";
-
-    const newToDo = document.createElement("li");
-    newToDo.appendChild(newToDoText);
-    newToDo.appendChild(markCompleteButton);
-    toDoListElement.appendChild(newToDo);
-
-    if(toDoStatus[index]){
-      newToDoText.classList.add("complete");
-      markCompleteButton.value = "Mark as incomplete";
-      
-    }
+  for (const [index, task] of toDoList.entries()) {
+    const newTodoTaskElement = createNewTodoItemElement(task, index);
+    toDoListElement.appendChild(newTodoTaskElement);
   }
 }
 
-// function createNewTodoItemElement(task, index) {}
+function createNewTodoItemElement(task, index) {
+  const newToDoTaskTextElement = document.createElement("p");
+  newToDoTaskTextElement.innerText = task;
+
+  const newTodoTaskElement = document.createElement("li");
+  newTodoTaskElement.appendChild(newToDoTaskTextElement);
+
+  const completeButtonElement = document.createElement("input");
+  completeButtonElement.type = "button";
+  completeButtonElement.value = "Completed";
+  completeButtonElement.onclick = function () {
+    toggleComplete(index);
+  };
+
+  if(toDoTaskStatus[index]) {
+    newToDoTaskTextElement.classList.add("complete");
+    completeButtonElement.value = "Mark as incomplete";
+  }
+
+  newTodoTaskElement.appendChild(completeButtonElement);
+  return newTodoTaskElement;
+}
 
 function toggleComplete(index) {
-  toDoStatus[index] = !toDoStatus[index];
+  toDoTaskStatus[index] = !toDoTaskStatus[index];
   updateTodoList();
 }
 
-updateTodoList();
 
 function keypress(inputId, buttonId) {
   const newItem = document.getElementById(inputId);
